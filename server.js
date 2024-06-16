9771b7949a7baf6a662764a435054d8e6b516383
shankarjatin <shankarjatin1005@gmail.com>
2024-06-16 17:52:39 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/candidates', require('./routes/candidates'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
