0d2dcde376526f84de882e04e38ed512f950022b
shankarjatin <shankarjatin1005@gmail.com>
2024-06-18 18:51:43 +0530

    error solved

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -1,14 +1,20 @@
 const Candidate = require('../models/Candidate');
 
 exports.searchCandidates = async (req, res) => {
-  const { location, jobRole } = req.query;
-  try {
-    const candidates = await Candidate.find({ location, jobRole });
-    res.json(candidates);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
+    try {
+        let query = {};
+        if (req.query.location) {
+          query.location = req.query.location;
+        }
+        if (req.query.jobRole) {
+          query.jobRole = req.query.jobRole;
+        }
+        const candidates = await Candidate.find(query);
+        res.json(candidates);
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+      }
 };
 
 exports.saveCandidate = async (req, res) => {
